ed91240da289599a66fc08881c1d22643eb353b7
dtan4 <dtanshi45@gmail.com>
2013-09-27 03:24:26 +0900

    Change variables name

--- lib/nowtv/client.rb
+++ lib/nowtv/client.rb
@@ -30,8 +30,8 @@ module Nowtv
         {
           station: program["station_name"],
           title: program["title"].gsub('　', ' '), # replace full-width space to half-width space
-          start: Time.parse(program["start"]).strftime("%H:%M"),
-          end: Time.parse(program["end"]).strftime("%H:%M")
+          start_time: Time.parse(program["start"]).strftime("%H:%M"),
+          end_time: Time.parse(program["end"]).strftime("%H:%M")
         }
       end
     end
