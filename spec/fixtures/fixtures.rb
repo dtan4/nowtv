56ef3ef128379047e050c851b3d7f45ddf3ca9b2
dtan4 <dtanshi45@gmail.com>
2013-10-02 04:45:31 +0900

    Separate fixtures

--- spec/fixtures/fixtures.rb
+++ spec/fixtures/fixtures.rb
@@ -1,80 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'spec_helper'
-require 'webmock/rspec'
-
-WebMock.allow_net_connect!
-
-API_URL = 'http://asp.tvguide.or.jp/api/broadcasting?ccode=goo&region_code='
-REGION_URL = 'http://asp.tvguide.or.jp/api/regions?ccode=goo'
-
-module Nowtv
-  describe Client do
-    before(:all) do
-      load File.expand_path("../../fixtures/fixtures.rb", __dir__)
-      @client = Nowtv::Client.new
-      @valid_region_code = 'tokyo'
-      @invalid_region_code = 'nihon'
-    end
-
-    before do
-      stub_request(:get, API_URL + @valid_region_code)
-        .to_return(body: EXAMPLE_VALID_PROGRAMS_BODY, status: 200)
-      stub_request(:get, API_URL + @invalid_region_code)
-        .to_return(body: EXAMPLE_INVALID_PROGRAMS_BODY, status: 200)
-      stub_request(:get, REGION_URL).to_return(body: EXAMPLE_REGION_BODY, status: 200)
-    end
-
-    describe '#get_programs' do
-      context 'with valid region code' do
-        subject { @client.get_programs(@valid_region_code) }
-        it { should be_instance_of Array }
-        it { should have_at_least(1).items }
-      end
-
-      context 'with invalid region' do
-        subject { @client.get_programs(@invalid_region_code) }
-        it { should be_instance_of Array }
-        it { should have(0).items }
-      end
-    end
-
-    describe '#get_program_list' do
-      context 'with valid region code' do
-        subject { @client.get_program_list(@valid_region_code) }
-        it { should be_instance_of Array }
-        it 'should be equal to example data' do
-          should eql EXAMPLE_RESTRUCTED_PROGRAMS
-        end
-      end
-
-      context 'with invalid region code' do
-        it 'should put error message in STDERR' do
-          $stderr.should_receive(:puts).with('Failed to get programs!')
-          @program_list = @client.get_program_list(@invalid_region_code)
-        end
-      end
-    end
-
-    describe '#restrct_program_list' do
-      subject { @client.restruct_program_list(EXAMPLE_PROGRAMS) }
-      it { should be_instance_of Array }
-      it 'should be equal to example data' do
-        should eql EXAMPLE_RESTRUCTED_PROGRAMS
-      end
-    end
-
-    describe '#get_region_list' do
-      subject { @client.get_region_list }
-      it { should be_instance_of Array }
-      it 'should be equal to example data' do
-        should eql EXAMPLE_REGION_LIST
-      end
-    end
-  end
-end
-
-
-
 EXAMPLE_VALID_PROGRAMS_BODY = <<-EOS
 ({"error":0,"ccode":"goo","region_code":"tokyo","media":{"code":"02","name":"\u5730\u4e0a\u6ce2"},"region":{"code":"tokyo","name":"\u6771\u4eac"},"programs":[{"station_code":"02-*01","station_name":"NHK\u7dcf\u5408","start":"201309131930","end":"201309131955","title":"\u7279\u5831\u9996\u90fd\u570f"},{"station_code":"02-#01","station_name":"NHK E\u30c6\u30ec","start":"201309131925","end":"201309131948","title":"\u8ab2\u5916\u6388\u696d\u3000\u3088\u3046\u3053\u305d\u5148\u8f29\u30fb\u9078"},{"station_code":"02-NTV","station_name":"\u65e5\u672c\u30c6\u30ec\u30d3","start":"201309131900","end":"201309132054","title":"\u7dca\u6025\u51fa\u52d5\uff01\u9003\u8d70\u8eca\u3092\u8ffd\u3048\uff01\u4ea4\u901a\u8b66\u5bdf\u3000\u771f\u590f\u306e\u5927\u635c\u67fb\u7dda"},{"station_code":"02-EX ","station_name":"\u30c6\u30ec\u30d3\u671d\u65e5","start":"201309131900","end":"201309131954","title":"\u30c9\u30e9\u3048\u3082\u3093\u8a95\u751f\u65e5\uff11\u6642\u9593\u30b9\u30da\u30b7\u30e3\u30eb"},{"station_code":"02-TBS","station_name":"TBS\u30c6\u30ec\u30d3","start":"201309131900","end":"201309131956","title":"\u7206\u5831\uff01\uff34\uff28\uff25\u3000\u30d5\u30e9\u30a4\u30c7\u30fc"},{"station_code":"02-TX ","station_name":"\u30c6\u30ec\u30d3\u6771\u4eac","start":"201309131830","end":"201309131950","title":"\u304a\u91d1\u304c\u306a\u304f\u3066\u3082\u5e78\u305b\u30e9\u30a4\u30d5\u3000\u304c\u3093\u3070\u308c\u30d7\u30a2\u30fc\u30ba\uff01"},{"station_code":"02-CX","station_name":"\u30d5\u30b8\u30c6\u30ec\u30d3","start":"201309131900","end":"201309131957","title":"\u30da\u30b1\u30dd\u30f3"},{"station_code":"02-MX ","station_name":"TOKYO MX","start":"201309131930","end":"201309132000","title":"\u3081\u305e\u3093\u4e00\u523b"},{"station_code":"02-HDT","station_name":"\u653e\u9001\u5927\u5b66\u30c6\u30ec\u30d3","start":"201309131900","end":"201309131945","title":"\u6587\u5316\u4eba\u985e\u5b66\u7b2c\uff11\uff11\u56de"}]})
 EOS
@@ -114,52 +38,52 @@ EXAMPLE_REGION_BODY = <<-EOS
 EOS
 
 EXAMPLE_REGION_LIST =
-  [
-   {:name=>"北海道", :code=>"hokk"},
-   {:name=>"青森", :code=>"aomori"},
-   {:name=>"岩手", :code=>"iwate"},
-   {:name=>"宮城", :code=>"miya"},
-   {:name=>"秋田", :code=>"akita"},
-   {:name=>"山形", :code=>"yamagata"},
-   {:name=>"福島", :code=>"fuku"},
-   {:name=>"茨城", :code=>"ibaraki"},
-   {:name=>"栃木", :code=>"tochigi"},
-   {:name=>"群馬", :code=>"gunma"},
-   {:name=>"埼玉", :code=>"saitama"},
-   {:name=>"千葉", :code=>"chiba"},
-   {:name=>"東京", :code=>"tokyo"},
-   {:name=>"神奈川", :code=>"kanagawa"},
-   {:name=>"新潟", :code=>"niigata"},
-   {:name=>"富山", :code=>"toyama"},
-   {:name=>"石川", :code=>"ishikawa"},
-   {:name=>"福井", :code=>"fukui"},
-   {:name=>"山梨", :code=>"yamanasi"},
-   {:name=>"長野", :code=>"nagano"},
-   {:name=>"岐阜", :code=>"gifu"},
-   {:name=>"静岡", :code=>"sizu"},
-   {:name=>"愛知", :code=>"aichi"},
-   {:name=>"三重", :code=>"mie"},
-   {:name=>"滋賀", :code=>"siga"},
-   {:name=>"京都", :code=>"kyoto"},
-   {:name=>"大阪", :code=>"osaka"},
-   {:name=>"兵庫", :code=>"hyougo"},
-   {:name=>"奈良", :code=>"nara"},
-   {:name=>"和歌山", :code=>"wakayama"},
-   {:name=>"鳥取", :code=>"tottori"},
-   {:name=>"島根", :code=>"shimane"},
-   {:name=>"岡山", :code=>"okayama"},
-   {:name=>"広島", :code=>"hirosima"},
-   {:name=>"山口", :code=>"yamagchi"},
-   {:name=>"徳島", :code=>"tokusima"},
-   {:name=>"香川", :code=>"kagawa"},
-   {:name=>"愛媛", :code=>"ehime"},
-   {:name=>"高知", :code=>"kouchi"},
-   {:name=>"福岡", :code=>"fukuoka"},
-   {:name=>"佐賀", :code=>"saga"},
-   {:name=>"長崎", :code=>"nagasaki"},
-   {:name=>"熊本", :code=>"kumamoto"},
-   {:name=>"大分", :code=>"oita"},
-   {:name=>"宮崎", :code=>"miyazaki"},
-   {:name=>"鹿児島", :code=>"kagosima"},
-   {:name=>"沖縄", :code=>"nawa"}
-  ]
+  {
+   北海道: "hokk",
+   青森: "aomori",
+   岩手: "iwate",
+   宮城: "miya",
+   秋田: "akita",
+   山形: "yamagata",
+   福島: "fuku",
+   茨城: "ibaraki",
+   栃木: "tochigi",
+   群馬: "gunma",
+   埼玉: "saitama",
+   千葉: "chiba",
+   東京: "tokyo",
+   神奈川: "kanagawa",
+   新潟: "niigata",
+   富山: "toyama",
+   石川: "ishikawa",
+   福井: "fukui",
+   山梨: "yamanasi",
+   長野: "nagano",
+   岐阜: "gifu",
+   静岡: "sizu",
+   愛知: "aichi",
+   三重: "mie",
+   滋賀: "siga",
+   京都: "kyoto",
+   大阪: "osaka",
+   兵庫: "hyougo",
+   奈良: "nara",
+   和歌山: "wakayama",
+   鳥取: "tottori",
+   島根: "shimane",
+   岡山: "okayama",
+   広島: "hirosima",
+   山口: "yamagchi",
+   徳島: "tokusima",
+   香川: "kagawa",
+   愛媛: "ehime",
+   高知: "kouchi",
+   福岡: "fukuoka",
+   佐賀: "saga",
+   長崎: "nagasaki",
+   熊本: "kumamoto",
+   大分: "oita",
+   宮崎: "miyazaki",
+   鹿児島: "kagosima",
+   沖縄: "nawa"
+}
